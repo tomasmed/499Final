de41969983190b8fe32f197ed3ee070c02f96249
Tomas <tomasmed@umich.edu>
2016-03-07 10:03:46 -0500

    First COmmit
    First Commit

---
+++ FinalProj/Assets/NPC/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public static GameController S; // Singleton class
+
+    public int team1_pts = 0;
+    public int team_2pts = 0;
+
+    void Awake()
+    {
+        S = this;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ FinalProj/Assets/NPC/Scripts/tick_Provisions.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tick_Provisions : MonoBehaviour {
+
+    //Varaibles
+    public float rate = 10; // The rate at which Provisions are sent to the destination
+    public GameObject provision_prefab; //The prefab for the provision.
+    public GameObject Destination; //The base to which Provisions aer being sent to.
+    public bool is_source = true;
+    public int team = 1;
+
+
+
+    //Provision management variables
+    public int max_provisions;
+    private int current_provisions = 0;
+
+    private Vector3 objective;
+    private float counter;
+
+    void Start()
+    {
+        counter = 0;
+        objective = Destination.transform.position - gameObject.transform.position; // The destination at which we shoot the provisions
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        if (is_source)
+        {
+            if (counter > rate && ((current_provisions - GameController.S.team1_pts) < max_provisions) )
+            {
+                current_provisions++;
+                counter = 0;
+
+                GameObject provision = Instantiate(provision_prefab) as GameObject;
+                provision.GetComponent<NavMeshAgent>().SetDestination(Destination.transform.position);
+                provision.GetComponent<Provision>().team = team;
+            }
+            else counter += Random.Range(0.2f,1.6f);
+        }
+	}
+}
