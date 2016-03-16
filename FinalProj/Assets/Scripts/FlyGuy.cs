ad5750997a11293b8d6f317af34b5e7a5e7ee14b
Benjamin Cooper <coobenja@umich.edu>
2016-03-16 18:47:23 -0400

    WIP Flying Dudes with Flocking

---
+++ FinalProj/Assets/Scripts/FlyGuy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class FlyGuy : MonoBehaviour {
+
+    Vector3 vel = Vector3.zero;
+    int neighborCount = 0;
+    ArrayList neighbors = new ArrayList();
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Vector3 computeAlignment() {
+        foreach(FlyGuy agent in neighbors)
+        {
+            if (agent != this.GetComponent<FlyGuy>())
+            {
+                if (Mathf.Abs(Vector3.Distance(agent.transform.position, this.transform.position)) < 10f)
+                {
+                    vel.x += agent.GetComponent<Rigidbody>().velocity.x;
+                    vel.y += agent.GetComponent<Rigidbody>().velocity.y;
+                    vel.z += agent.GetComponent<Rigidbody>().velocity.z;
+                    neighborCount++;
+                }
+
+            }
+
+        }
+
+        if (neighborCount == 0) { return vel; }
+
+        return Vector3.zero; //lmao
+
+    }
+
+}
