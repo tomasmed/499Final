2daac0e6bd3213c2100a6986da8c620b0bb0e7e6
Tomas <tomasmed@umich.edu>
2016-03-20 20:57:12 -0400

    Double Jump and simple gliding by pressing e

--- FinalProj/Assets/Character/MovementV2.cs
+++ FinalProj/Assets/Character/MovementV2.cs
@@ -6,9 +6,10 @@ public class MovementV2 : MonoBehaviour {
 	//Variables
 	public float speed = 1f; //Speed multiplier by which the character will move
 	public float JumpPower = 20;
+    public int glideStrength = 5; //Multiplier to see how much the cahracter glides
 
 
-	public bool _______________________;
+    public bool _______________________;
 
 	private float HorizIn = 0f; //Variable to hold the Horizontal Input from player
 	private float VertIn = 0f; //Variable to hold the Vertical Input from player
@@ -27,76 +28,96 @@ public class MovementV2 : MonoBehaviour {
 	//Jump Variables
 	public int grounded = 2;
 
-	//---------------------------------End of Variables
+    //Glide Variables
+    public bool gliding = false;
 
-	// Use this for initialization
-	void Start () {
+
+    //---------------------------------End of Variables
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	void OnCollisionEnter(Collision coll)
 	{
 		Debug.Log ("Collided with Something");
-		if (coll.gameObject.tag == "Ground")grounded = 2;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		//Obtain Input from player
-		HorizIn = Input.GetAxis ("Horizontal");
-		VertIn = Input.GetAxis("Vertical");
-		JumpIn = Input.GetKeyDown (KeyCode.Space);
-		GlideIn = Input.GetKeyDown (KeyCode.E);
-		//Debug.Log ("Vertical and Horizontal Inputs are: " + VertIn + " , " + HorizIn);
-		//
+        if (coll.gameObject.tag == "Ground")
+        {
+            grounded = 2;
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            gliding = false;
+        }
+    }
 
-		//Obtain the Camera direction that is passed to the character
-		if (Camera.main != null) 
-		{
-			CamDir = Vector3.Scale (Camera.main.transform.forward, new Vector3 (1, 0, 1)).normalized;
-			CamRelative = VertIn * CamDir + HorizIn * Camera.main.transform.right;
-		}
-		else Debug.Log ("Main Camera is Not attached to Player Controller");
+    // Update is called once per frame
+    void FixedUpdate() {
+        //Obtain Input from player
+        HorizIn = Input.GetAxis("Horizontal");
+        VertIn = Input.GetAxis("Vertical");
+        JumpIn = Input.GetKeyDown(KeyCode.Space);
+        GlideIn = Input.GetKeyDown(KeyCode.E);
+        //Debug.Log ("Vertical and Horizontal Inputs are: " + VertIn + " , " + HorizIn);
+        //
+
+        //Obtain the Camera direction that is passed to the character
+        if (Camera.main != null)
+        {
+            CamDir = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+            CamRelative = VertIn * CamDir + HorizIn * Camera.main.transform.right;
+        }
+        else Debug.Log("Main Camera is Not attached to Player Controller");
 
 
 
-		//Check if the character has received input to move
-		if (HorizIn != 0 || VertIn != 0) {
-			//Calculate the Rotation based on Horizontal Inputs
-			if (HorizIn != 0) {
-				Vector3 move = Vector3.one;
-				float TurnSpeed = Mathf.Lerp (180, 180, CamRelative.z);
-				TurnAmount = Mathf.Atan2 (move.x, move.z);
-				FinalRotation = + TurnAmount * TurnSpeed * Time.deltaTime * HorizIn;
-				
-				//Move and Rotate
-				transform.Rotate(0, (FinalRotation % 360), 0);
-			}
-			//Calculate the Forward movement based on Vertical Inputs
-			if (VertIn != 0) {
-				Vector3 temp = gameObject.GetComponent<Rigidbody>().velocity;
-				//gameObject.GetComponent<Rigidbody> ().velocity = (CamRelative) * speed;
-				gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(CamRelative.x , temp.y/speed ,CamRelative.z)*speed;
-			}
-		} 
-		else 
-		{
-			Vector3 temp = gameObject.GetComponent<Rigidbody>().velocity;
-			gameObject.GetComponent<Rigidbody>().velocity = new Vector3(temp.x , temp.y, temp.z);
-		}
+        //Check if the character has received input to move
+        if (HorizIn != 0 || VertIn != 0) {
+            //Calculate the Rotation based on Horizontal Inputs
+            if (HorizIn != 0) {
+                Vector3 move = Vector3.one;
+                float TurnSpeed = Mathf.Lerp(180, 180, CamRelative.z);
+                TurnAmount = Mathf.Atan2(move.x, move.z);
+                FinalRotation = +TurnAmount * TurnSpeed * Time.deltaTime * HorizIn;
 
-		//Check for input on jumping and Jump if appropriate
-		if (JumpIn && (grounded > 0))
-		{
-			Debug.Log ("Jumping!");
-			Vector3 temp = gameObject.GetComponent<Rigidbody>().velocity;
-			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (temp.x, JumpPower, temp.z);
-			grounded -= 1;
-			if (GlideIn)
-			{
-				gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity; 
-			}
-		}
-		gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                //Move and Rotate
+                transform.Rotate(0, (FinalRotation % 360), 0);
+            }
+            //Calculate the Forward movement based on Vertical Inputs
+            if (VertIn != 0) {
+                Vector3 temp = gameObject.GetComponent<Rigidbody>().velocity;
+                //gameObject.GetComponent<Rigidbody> ().velocity = (CamRelative) * speed;
+                gameObject.GetComponent<Rigidbody>().velocity = new Vector3(CamRelative.x, temp.y / speed, CamRelative.z) * speed;
+            }
+        }
+        else
+        {
+            Vector3 temp = gameObject.GetComponent<Rigidbody>().velocity;
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(temp.x, temp.y, temp.z);
+        }
+
+        //Check for input on jumping and Jump if appropriate
+        if (JumpIn && (grounded > 0))
+        {
+            Debug.Log("Jumping!");
+            Vector3 temp = gameObject.GetComponent<Rigidbody>().velocity;
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(temp.x, JumpPower, temp.z);
+            grounded -= 1;
+
+        }
+
+        //Check for gliding
+        if (GlideIn && (grounded < 1))
+        {
+            gliding = true;
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (gliding)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * glideStrength);
+
+        }
+
+        gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 		//Debug.Log (gameObject.GetComponent<Rigidbody> ().velocity);
 		
 	}
